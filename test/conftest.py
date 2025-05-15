20ef0b8dd6df9a8471ca2ba20c922174fc3c1c36
hoangduc <hoanggiaduc6990@gmail.com>
2025-05-15 15:44:51 +0700

    Update full project code

--- test/conftest.py
+++ test/conftest.py
@@ -1,6 +1,7 @@
 import os
 import sys
 
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../backend")))
 # Đảm bảo có thể import từ thư mục gốc
 BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 sys.path.insert(0, BASE_DIR)
