10dc73987788cd87943ad5fe7a4c3f79c344844c
hoangduc <hoanggiaduc6990@gmail.com>
2025-05-16 01:31:44 +0700

    yo

--- test/conftest.py
+++ test/conftest.py
@@ -6,4 +6,5 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../back
 BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 sys.path.insert(0, BASE_DIR)
 
-#cấu hình test chung
+# Thiết lập đường dẫn import, để các file test khác có thể import được module từ thư mục backend/
+# Không chứa test nào, chỉ hỗ trợ các file còn lại chạy đúng.
